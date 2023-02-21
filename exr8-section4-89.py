c0735f6b9479ab75e5654f1a013762686962a4cf
hilalipik <hilalipik@gmail.com>
2023-02-21 15:40:03 +0200

    8th exercise

---
+++ exr8-section4-89.py
@@ -0,0 +1,13 @@
+def highest_even(li):
+    '''
+    Function finds the highest number in list that is divisible by 2.
+    Input: list.
+    Output: the highest number in list that is divisible by 2.
+    '''
+    max = li[0]
+    for i in li:
+        if i > max and i %2==0:
+            max = i
+    return max
+
+print(highest_even([10,3,4,5,99,20,1]))
