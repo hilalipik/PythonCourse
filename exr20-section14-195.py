7ae82df3c6bc3f2224a2afff303f38ff6b98a63d
hilalipik <hilalipik@gmail.com>
2023-02-23 16:16:40 +0200

    20th exercise

--- exr20-section14-195.py
+++ exr20-section14-195.py
@@ -1,13 +1,15 @@
 import re
 
-pattern = re.compile(r"^[a-zA-Z0-9@#$%]{8,}$")
+pattern = re.compile(r"^[a-zA-Z0-9@#$%]{7,}[0-9]+$")
 
 while True:
     print('''- At least 8 characters long.
-- Only use a-z, numbers and @#$%.''')
+- Only use a-z, numbers and @#$%.
+- Must end with a number.''')
     password = input("Enter password: ")
     valid = pattern.match(password)
     if(valid):
         print("Ok!")
         break
     print("try again...\n")
+
