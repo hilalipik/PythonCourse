118f7576e05401579f669121395aca11af9564f3
hilalipik <hilalipik@gmail.com>
2023-02-28 11:27:38 +0200

    22nd exercise

---
+++ exr22-section17-213/JPGtoPNGconverter.py
@@ -0,0 +1,27 @@
+#         <script location>                     <source directory>         <destination directory>
+# exr22-section17-213/JPGtoPNGconverter.py exr22-section17-213/animals/ exr22-section17-213/new_folder/
+# example ^^
+
+from PIL import Image, ImageFilter
+import sys
+import os
+
+try:
+    source_dir = sys.argv[1]
+    dest_dir = sys.argv[2]
+except IndexError:
+    print("Must enter 2 parameters.")
+    exit()
+
+if not (os.path.exists(source_dir)):
+    print("The source path '" + source_dir + "' does not exist.")
+    exit()
+
+if not (os.path.exists(dest_dir)):
+    os.makedirs(dest_dir)
+
+for name in os.listdir(source_dir):
+    if name.endswith('jpg'):
+        img = Image.open(f'{source_dir}{name}')
+        name = name[:name.find('jpg')]
+        img.save(f'{dest_dir}{name}png', 'png')
